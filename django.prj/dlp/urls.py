a31ebea3551666bbf211732f432936a791438931
Agustin Rodriguez <arberoois@gmail.com>
2017-07-14 17:24:51 -0300

    agregado products url

--- django.prj/dlp/urls.py
+++ django.prj/dlp/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import url
-
-
 from views import home, contact, products, team
 
 
@@ -8,4 +6,5 @@ urlpatterns = [
     url('^$', home),
     url('^contact/', contact), 
     url('^team/', team),
+    url('^products', products), 
 ]
