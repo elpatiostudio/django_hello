1498c3341ba4f9dba1bfb7a8fa71261c2b86b265
Agustin Rodriguez <arberoois@gmail.com>
2017-07-21 16:20:09 -0300

    Install CSS Framework Materializecss from CDN

--- django.prj/dlp/urls.py
+++ django.prj/dlp/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from views import home, contact, products, team
+from views import home, contact, products, team, members
 
 
 urlpatterns = [
     url('^$', home),
     url('^contact/', contact), 
     url('^team/', team),
-    url('^products', products), 
+    url('^products', products),
+    url('^members', members), 
 ]
