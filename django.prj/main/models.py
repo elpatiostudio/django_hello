dc8c7e0f09aa609bec7511769f249e5b0763562e
Agustin Rodriguez <arberoois@gmail.com>
2017-08-04 15:25:28 -0300

    migration 003

--- django.prj/main/models.py
+++ django.prj/main/models.py
@@ -6,14 +6,14 @@ from django.db import models
 
 class Member(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
-    instrument = models.CharField(max_length=200)
+    description = models.TextField(default='')
+    instrument = models.CharField(max_length=200, default='')
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Other'),
     )
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=GENDER_CHOICES[0][0])
+
 
     
