af93055571a1bd4bbb9f959c9062fb72486a3109
Clemente Berro <clementeberro@gmail.com>
2017-08-04 17:51:53 -0300

    Agrego campos a products

--- django.prj/products/admin.py
+++ django.prj/products/admin.py
@@ -4,5 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from models import Product
 
+class ProductAdmin(admin.ModelAdmin):
+    # fields = ('name', 'instrument',)
+    list_display = ('name', 'type_of_product', 'price' )
+    search_fields = ('name', 'description', 'type_of_products',)
+    list_filter = ('type_of_product',)
 
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
