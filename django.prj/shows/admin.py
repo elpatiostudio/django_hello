047b49a5c145a430922a0f6f7ce43be659cea1bb
Caterina Knobel <caterinavioleta@gmail.com>
2017-08-04 17:45:32 -0300

    Shows create models

--- django.prj/shows/admin.py
+++ django.prj/shows/admin.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from models import Show
+
+
+class ShowAdmin(admin.ModelAdmin):
+    # fields = ('name', 'instrument',)
+    list_display = ('location', 'name', 'date', 'description')
+    search_fields = ('name', 'location', 'date',)
+    list_filter = ('location',)
+
+admin.site.register(Show, ShowAdmin)
