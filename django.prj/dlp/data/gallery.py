716b014617ad9f2304f2bd2b10f45cd93d264919
Joaco Stirling <wise@192.168.1.47>
2017-08-08 22:13:32 -0300

    Gallery displaying image

--- django.prj/dlp/data/gallery.py
+++ django.prj/dlp/data/gallery.py
@@ -1,23 +1,40 @@
 GALLERY = [{
-			 "image": "http://via.placeholder.com/200x200",
-			 
-			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
-			 
-			 },
-			 { 			 "image": "http://via.placeholder.com/200x200",
-			 
+			 "image": "https://upload.wikimedia.org/wikipedia/commons/d/d5/Pic_de_neige_cordier_Face_E.jpg",
 			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
 			 
 			 },
 			 { 
 			 "image": "http://via.placeholder.com/200x200",
-			 
 			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
 			 
 			 },
 			 { 
 			 "image": "http://via.placeholder.com/200x200",
+			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
 			 
+			 },
+			 { 
+			 "image": "http://via.placeholder.com/200x200",
+			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
+			 
+			 },
+			 {
+			 "image": "http://via.placeholder.com/200x200",
+			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
+			 
+			 },
+			 { 
+			 "image": "http://via.placeholder.com/200x200",
+			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
+			 
+			 },
+			 { 
+			 "image": "http://via.placeholder.com/200x200",
+			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
+			 
+			 },
+			 { 
+			 "image": "http://via.placeholder.com/200x200",
 			 "description": "Cillum et elit esse dolor duis nisi aliquip velit velit id cupidatat deserunt.",
 			 
 			 },]
