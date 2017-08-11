83cea7b532dd77815a909e07d58927d80adca91c
tadeo <tadeo@tadeocarrier.com>
2017-08-11 16:04:02 -0300

    Add products Category model and a FK from products

--- django.prj/products/models.py
+++ django.prj/products/models.py
@@ -4,18 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    TYPE_CHOICES = (
-        ('A', 'Apparel'),
-        ('D', 'DVDs'),
-        ('C', 'CDs'),
-        ('O', 'Others'),
-        )
-    type_of_product = models.CharField(max_length=1, choices=TYPE_CHOICES,)
+    type_of_product = models.ForeignKey(Category)
     description = models.TextField(default='', null=True, blank=True,)
     price = models.CharField(max_length=200, default='Not avaliable at the moment')
 
     def __unicode__(self):
         return self.name
-
