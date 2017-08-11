b061b02f492696851ed40638f73913bc2fe8e83b
Clemente Berro <clementeberro@gmail.com>
2017-08-11 16:26:44 -0300

    Add price on dispayed message

--- django.prj/products/models.py
+++ django.prj/products/models.py
@@ -13,7 +13,8 @@ class Product(models.Model):
     	('O', 'Others'),
     	)
     type_of_product = models.CharField(max_length=1, choices=TYPE_CHOICES,) 
-    description = models.TextField(default='', null=True, blank=True,)
+    description = models.TextField(null=True, blank=True, default='No info',)
+    #image = models.ImageField(upload_to='None', blank=True, null=True, verbose_name=_("Image"))
     price = models.CharField(max_length=200, default='Not avaliable at the moment')
 
     def __unicode__(self):
